ad3e375b209180cf3e59c26f14b0178e5e2cc4cb
migsking <migsking@naver.com>
2019-07-03 23:50:03 +0900

    [CREATE] Register Management

--- app/views/api/v1/user/auth/register.py
+++ app/views/api/v1/user/auth/register.py
@@ -1,13 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort
 from flask_restful import Api
 from app.views import BaseResource
+from app.models.user import UserModel
 
 blueprint = Blueprint(__name__,__name__)
 api = Api(blueprint)
 
 @api.resource('/auth/register')
 class RegisterManagement(BaseResource):
-    def get(self):
-        return {
-            'TEST':'TEST'
-        }
+    def post(self):
+        '''
+        유저 회원가입
+        '''
+        id = request.json['id']
+        pw = request.json['pw']
+        pw_check = request.json['pw_check']
+        name = request.json['name']
+
+        if not pw == pw_check:
+            abort(406)
+
+        if UserModel.objects(id=id).first():
+            abort(406)
+
+        UserModel(
+            id = id,
+            pw = pw,
+            name = name,
+        ).save()
+
+        return '', 201
